0c0b7c947b83f417c6f63a33fe2f53a87d01e1a8
IsmaIgnacio <24005361@uagro.mx>
2026-04-19 19:18:42 -0600

    Primer commit API

---
+++ Controllers/AlertasController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NanoGuardian.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AlertasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObtenerAlerta()
+        {
+            return Ok(new
+            {
+                paciente = "Ismael",
+                fuerzaImpactoG = 8,
+                estado = "Emergencia"
+            });
+        }
+    }
+}
